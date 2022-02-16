324961c03e23748881d874116f5268579b4a7f58
Hyeyoung <96829798+ebulsok@users.noreply.github.com>
2022-02-16 21:33:57 +0900

    Update app.py
    잘못 옮겨놔서 코드 2줄을 빼먹었습니다..

--- music-temp/app.py
+++ music-temp/app.py
@@ -3,6 +3,9 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient('localhost', 27017)
+db = client.makingproject
+
 @app.route('/')
 def home():
     return render_template('search.html')
