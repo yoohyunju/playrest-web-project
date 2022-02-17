af3bf6d3230feb967f000629c1c8d8be3d4c2e97
yoohyunju <yhj2756@naver.com>
2022-02-17 14:33:25 +0900

    모든 페이지 헤더 추가
    모든 페이지에 헤더 추가하였습니다.

--- app.py
+++ app.py
@@ -86,32 +86,5 @@ def addMusic():
         '$push': {'playlist_music': {'music_title': title_receive, 'music_artist': artist_receive}}});
     return jsonify({'msg': '추가했습니당'})
 
-'''
-# 주문하기(POST) API
-@app.route('/order', methods=['POST'])
-def save_order():
-    name_receive = request.form['name_give']
-    count_receive = request.form['count_give']
-    address_receive = request.form['address_give']
-    phone_receive = request.form['phone_give']
-
-    doc = {
-        'name': name_receive,
-        'count': count_receive,
-        'address': address_receive,
-        'phone': phone_receive
-    }
-    db.orders.insert_one(doc)
-
-    return jsonify({'result': 'success', 'msg': '주문 완료!'})
-
-
-# 주문 목록보기(Read) API
-@app.route('/order', methods=['GET'])
-def view_orders():
-    orders = list(db.orders.find({}, {'_id': False}))
-    return jsonify({'result': 'success', 'orders': orders})
-
-'''
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
